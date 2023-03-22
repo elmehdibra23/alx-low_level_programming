e75546f04fb0ea5d9c68e97d52c8f871f4c50c39
brakine elmehdi <elmehdibra@gmail.com>
2023-03-21 17:44:26 -0700

    fibonacci4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,69 +1,17 @@
 #include <stdio.h>
-#define MAX_DIGITS 500
-/**
- * print_string - Prints a string of digits
- * @str: The string to print
- */
-void print_string(char str[MAX_DIGITS])
-{
-int i = 0;
-while (i < MAX_DIGITS && str[i] == 0)
-i++;
-if (i == MAX_DIGITS)
-putchar('0');
-else
-while (i < MAX_DIGITS)
-putchar(str[i++] + '0');
-}
-/**
- * add_strings - Adds two strings of digits
- * @a: The first string to add
- * @b: The second string to add
- * @res: The resulting sum
- */
-void add_strings(char a[MAX_DIGITS], char b[MAX_DIGITS], char res[MAX_DIGITS])
-{
-int i;
-char carry = 0;
-for (i = MAX_DIGITS - 1; i >= 0; i--)
-{
-res[i] = a[i] + b[i] + carry;
-if (res[i] > 9)
-{
-res[i] -= 10;
-carry = 1;
-}
-else
-carry = 0;
-}
-}
-/**
- * main - Prints the first 98 Fibonacci numbers
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-char prev[MAX_DIGITS] = {0};
-char curr[MAX_DIGITS] = {0};
-char next[MAX_DIGITS];
-int i;
-prev[MAX_DIGITS - 1] = 1;
-curr[MAX_DIGITS - 1] = 2;
-printf("1, ");
-print_string(prev);
-printf(", ");
-print_string(curr);
-for (i = 3; i <= 98; i++)
+int count, n1 = 1, n2 = 2, next;
+printf("%d, %d, ", n1, n2);
+for (count = 3; count <= 98; count++)
 {
-add_strings(prev, curr, next);
-printf(", ");
-print_string(next);
-for (int j = 0; j < MAX_DIGITS; j++)
-prev[j] = curr[j];
-for (int j = 0; j < MAX_DIGITS; j++)
-curr[j] = next[j];
+next = n1 + n2;
+n1 = n2;
+n2 = next;
+if (count != 98)
+printf("%d, ", next);
+else
+printf("%d\n", next);
 }
-printf("\n");
-return (0);
+return 0;
 }
