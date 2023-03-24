8939b1646e172de229148d96cf69f03972e813d8
brakine elmehdi <elmehdibra@gmail.com>
2023-03-23 18:40:51 -0700

    printtriangle100ffa

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
 /**
  * main - finds and prints the largest prime factor of 612852475143
  *
@@ -8,29 +7,24 @@
  */
 int main(void)
 {
-    long num = 612852475143;
-    long max_prime = -1;
-    int i;
-
-    while (num % 2 == 0)
-    {
-        max_prime = 2;
-        num /= 2;
-    }
-
-    for (i = 3; i <= sqrt(num); i += 2)
-    {
-        while (num % i == 0)
-        {
-            max_prime = i;
-            num /= i;
-        }
-    }
-
-    if (num > 2)
-        max_prime = num;
-
-    printf("%ld\n", max_prime);
-
-    return (0);
+long num = 612852475143;
+long max_prime = -1;
+int i;
+while (num % 2 == 0)
+{
+max_prime = 2;
+num /= 2;
+}
+for (i = 3; i <= sqrt(num); i += 2)
+{
+while (num % i == 0)
+{
+max_prime = i;
+num /= i;
+}
+}
+if (num > 2)
+max_prime = num;
+printf("%ld\n", max_prime);
+return (0);
 }
