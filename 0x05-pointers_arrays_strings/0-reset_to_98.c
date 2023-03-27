950bbc64cb72f73222ded68d25ed29869c1d9616
brakine elmehdi <elmehdibra@gmail.com>
2023-03-26 21:04:05 -0700

    resettonintyeight

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,12 @@
 #include "main.h"
-/*
-* reset to ninty-eight
-*/
+
+/**
+ * reset_to_98 - updates the value of an int to 98
+ * @n: pointer to the int to update
+ *
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+    *n = 98;
 }
