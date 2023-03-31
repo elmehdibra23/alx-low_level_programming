9718586e0c971a73dcdb5c554915b4a9468b732a
brakine elmehdi <elmehdibra@gmail.com>
2023-03-30 19:30:55 -0700

    rotupdate

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#define ROT 13
 /**
  * rot13 - Encodes a string using rot13.
+ *
  * @s: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
@@ -11,18 +13,11 @@ char *c = s;
 int i;
 while (*c)
 {
-if ((*c >= 'a' && *c <= 'm') || (*c >= 'A' && *c <= 'M'))
-{
-for (i = 0; i < 13; i++)
-(*c)++;
-}
-else if ((*c >= 'n' && *c <= 'z') || (*c >= 'N' && *c <= 'Z'))
-{
-for (i = 0; i < 13; i++)
-(*c)--;
-}
+for (i = 0; ((*c >= 'a' && *c <= 'm') || (*c >= 'A' && *c <= 'M')) && i < ROT; i++)
+*c += 1;
+for (i = 0; ((*c >= 'n' && *c <= 'z') || (*c >= 'N' && *c <= 'Z')) && i < ROT; i++)
+*c -= 1;
 c++;
 }
-return (s);
+return s;
 }
-
