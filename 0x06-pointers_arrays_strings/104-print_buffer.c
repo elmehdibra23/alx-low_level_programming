8288dbb117389089ce4f9e6f039b988be8dacf32
brakine elmehdi <elmehdibra@gmail.com>
2023-03-31 18:54:30 -0700

    printbuffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", *(b + i + j));
+			else
+				printf("  ");
+			if ((j + 1) % 2 == 0)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (*(b + i + j) >= ' ' && *(b + i + j) <= '~')
+					printf("%c", *(b + i + j));
+				else
+					printf(".");
+			}
+		}
+		printf("\n");
+	}
+}
