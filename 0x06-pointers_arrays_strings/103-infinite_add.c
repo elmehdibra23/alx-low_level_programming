2817c7c44545bf7275bfecdc27b0ee57c321ad8a
brakine elmehdi <elmehdibra@gmail.com>
2023-03-31 18:48:06 -0700

    updateinfiniteadd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,41 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * infinite_add - Adds two numbers.
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store result
+ * @size_r: size of buffer
  *
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: A buffer to store the result.
- * @size_r: The size of the buffer.
- *
- * Return: A pointer to the result. If the result cannot be stored in the buffer,
- *         return 0.
+ * Return: pointer to result or 0 if result cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, len, i, j, k, carryover;
-	char digit;
+    int i, j, n1_len, n2_len, max_len, sum, remainder = 0;
 
-	len1 = len2 = len = 0;
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
+    for (n1_len = 0; n1[n1_len]; n1_len++)
+        ;
+    for (n2_len = 0; n2[n2_len]; n2_len++)
+        ;
 
-	if (len1 > size_r || len2 > size_r)
-		return (0);
+    max_len = n1_len > n2_len ? n1_len : n2_len;
+    if (size_r <= max_len + 1)
+        return (0);
 
-	i = len1 - 1, j = len2 - 1, k = 0, carryover = 0;
-	while (1)
-	{
-		if (i < 0 && j < 0 && carryover == 0)
-			break;
-		digit = carryover;
-		if (i >= 0)
-			digit += n1[i--] - '0';
-		if (j >= 0)
-			digit += n2[j--] - '0';
-		if (k >= size_r)
-			return (0);
-		carryover = digit > 9 ? 1 : 0;
-		r[k++] = digit % 10 + '0';
-	}
-
-	if (k >= size_r)
-		return (0);
-
-	for (i = k - 1; i >= 0; i--)
-		_putchar(r[i]);
-	_putchar('\n');
-
-	return (r);
+    r[max_len + 1] = '\0';
+    for (i = n1_len - 1, j = n2_len - 1; i >= 0 || j >= 0 || remainder; i--, j--)
+    {
+        sum = remainder;
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
+        r[max_len--] = sum % 10 + '0';
+        remainder = sum / 10;
+    }
+    if (max_len == 0)
+        return (r + 1);
+    r[max_len] = remainder + '0';
+    return (r);
 }
-
