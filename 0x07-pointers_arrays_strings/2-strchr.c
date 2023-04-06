1e8b6110883ab7bdef4450823436c691a39c7964
brakine elmehdi <elmehdibra@gmail.com>
2023-04-05 20:13:38 -0700

    updatestrchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 /**
  * _strchr - Locates a character in a string.
+ *
  * @s: The string to search.
  * @c: The character to locate.
  *
@@ -9,18 +10,18 @@
  *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
-{
+{   
 while (*s != '\0')
 {
 if (*s == c)
 {
-return s;
+return (s);
 }
 s++;
 }
 if (c == '\0')
 {
-return s;
+return (s);
 }
-return NULL;
+return (NULL);
 }
