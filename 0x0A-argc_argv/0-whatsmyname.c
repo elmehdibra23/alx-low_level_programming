b3ad64fe21717222700ca0bed28c9ba3c49866cd
brakine elmehdi <elmehdibra@gmail.com>
2023-04-11 18:29:05 -0700

    whats_my_name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: Always always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
