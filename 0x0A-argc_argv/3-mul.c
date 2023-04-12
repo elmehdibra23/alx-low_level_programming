7c1ea28017177cc48d169906e28bb16cc9ab15c7
brakine elmehdi <elmehdibra@gmail.com>
2023-04-11 18:58:50 -0700

    updatemulc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * @argc: incoming argument
- * @argv: incoming argument
- *
- * Return: Always 0 (Success)
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int total;
-if (argc < 2)
+int num1, num2, product;
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-total = (int) *argv[1] * (int) *argv[2];
-printf("%d\n", total);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+product = num1 * num2;
+printf("%d\n", product);
 return (0);
 }
