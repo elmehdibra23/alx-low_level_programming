b09e612ff5715e2dc7a052154725ba6af06c4e76
brakine elmehdi <elmehdibra@gmail.com>
2023-04-11 18:53:13 -0700

    multiupdatsjhhhhdz

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
- * @argc: Number of arguments passed to the program
- * @argv: Array of arguments passed to the program
  *
- * Return: 1 if there are less than 2 arguments, 0 otherwise
+ * @argc: incoming argument
+ * @argv: incoming argument
+ *
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int num1, num2, result;
-if (argc < 3)
+int total;
+if (argc < 2)
 {
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n", result);
+total = (int) *argv[1] * (int) *argv[2];
+printf("%d\n", total);
 return (0);
 }
