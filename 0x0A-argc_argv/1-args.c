1fc3ffbbb42b434a36a0ceebf62c0374aea3817c
brakine elmehdi <elmehdibra@gmail.com>
2023-04-11 18:31:27 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: Mas always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
