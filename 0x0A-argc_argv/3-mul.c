24b2b6b4c8087a1457f7a1f7d6c5686fdf74ec86
brakine elmehdi <elmehdibra@gmail.com>
2023-04-13 18:23:59 -0700

    updatemulthrddd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,39 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success, 1 otherwise
+ */
 int main(int argc, char *argv[])
 {
-    int i, j, flag = 0;
-    int a, b;
-
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
-    for (i = 1; argv[i]; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-            {
-                flag = 1;
-                break;
-            }
-        }
-    }
-
-    if (flag == 1)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
-    printf("%d\n", a * b);
-
-    return 0;
+int a, b;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (0);
 }
 
