cc9cd4692e51f0f913c21f2707983b9f37515b84
brakine elmehdi <elmehdibra@gmail.com>
2023-04-13 17:46:38 -0700

    updateaddc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+/**
+ * _isdigit - Check if a character is a digit
+ * @c: Character to check
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    return (0);
+}
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: 1 if there is a non-digit character in the arguments, 0 otherwise
+ */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
+    int i, j, sum = 0;
+
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j]; j++)
+        {
+            if (!_isdigit(argv[i][j]))
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+    printf("%d\n", sum);
+
+    return (0);
 }
-for (i = 1; i < argc; i++)
-{
-int j;
-for (j = 0; argv[i][j]; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
-}
+
