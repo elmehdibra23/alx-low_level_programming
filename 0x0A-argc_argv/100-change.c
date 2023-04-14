b73b3287bedc9a54bde925f4e03bbde77b1c7312
brakine elmehdi <elmehdibra@gmail.com>
2023-04-13 18:30:00 -0700

    100change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - program that prints the minimum number of coins
  * to make change for an amount of money.
@@ -10,48 +9,43 @@
  */
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
-
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    cents = atoi(argv[1]);
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
-
-    while (cents >= 25)
-    {
-        cents -= 25;
-        coins++;
-    }
-    while (cents >= 10)
-    {
-        cents -= 10;
-        coins++;
-    }
-    while (cents >= 5)
-    {
-        cents -= 5;
-        coins++;
-    }
-    while (cents >= 2)
-    {
-        cents -= 2;
-        coins++;
-    }
-    while (cents >= 1)
-    {
-        cents -= 1;
-        coins++;
-    }
-
-    printf("%d\n", coins);
-    return (0);
+int cents, coins = 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
-
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+while (cents >= 25)
+{
+cents -= 25;
+coins++;
+}
+while (cents >= 10)
+{
+cents -= 10;
+coins++;
+}
+while (cents >= 5)
+{
+cents -= 5;
+coins++;
+}
+while (cents >= 2)
+{
+cents -= 2;
+coins++;
+}
+while (cents >= 1)
+{
+cents -= 1;
+coins++;
+}
+printf("%d\n", coins);
+return (0);
+}
