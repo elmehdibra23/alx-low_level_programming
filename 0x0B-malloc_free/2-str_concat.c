e85fd08d7509adfdf36713e8b540bd9c72edfc97
brakine elmehdi <elmehdibra@gmail.com>
2023-04-16 17:40:49 -0700

    strconcatupdate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * str_concat - concatenates two strings
  * @s1: first string
@@ -12,29 +11,23 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i, j, len1 = 0, len2 = 0;
-    char *concat;
-
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-
-    for (i = 0; s1[i]; i++)
-        len1++;
-    for (j = 0; s2[j]; j++)
-        len2++;
-
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (concat == NULL)
-        return (NULL);
-
-    for (i = 0; s1[i]; i++)
-        concat[i] = s1[i];
-    for (j = 0; s2[j]; j++)
-        concat[i + j] = s2[j];
-    concat[i + j] = '\0';
-
-    return (concat);
+int i, j, len1 = 0, len2 = 0;
+char *concat;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i]; i++)
+len1++;
+for (j = 0; s2[j]; j++)
+len2++;
+concat = malloc(sizeof(char) * (len1 + len2 + 1));
+if (concat == NULL)
+return (NULL);
+for (i = 0; s1[i]; i++)
+concat[i] = s1[i];
+for (j = 0; s2[j]; j++)
+concat[i + j] = s2[j];
+concat[i + j] = '\0';
+return (concat);
 }
-
