ba5fdc48d19944d10c6b86bff7862e5f5f16529b
brakine elmehdi <elmehdibra@gmail.com>
2023-04-17 17:42:33 -0700

    update_dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-/* Define a new type struct dog with the following elements:
- * - name, type = char *
- * - age, type = float
- * - owner, type = char *
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: Pointer to char representing the dog's name
+ * @age: Float representing the dog's age
+ * @owner: Pointer to char representing the dog's owner
+ *
+ * Description: A structure named "dog" with three elements: name, age, and owner.
+ * The name and owner elements are pointers to char (char *), and the age element
+ * is of type float.
  */
 struct dog
 {
