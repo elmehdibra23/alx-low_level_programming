fcaa7fc24c5de30b872914ad3096391e323d9f85
brakine elmehdi <elmehdibra@gmail.com>
2023-04-24 13:51:51 -0700

    upudate

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,8 +11,8 @@ typedef struct print_fmt
 char *type;
 void (*func)(va_list);
 } print_fmt_t;
-/* function prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #endif /* VARIADIC_FUNCTIONS_H */
