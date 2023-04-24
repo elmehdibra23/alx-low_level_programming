35381111d6cbdcf9fc48392496ee5d31d9b02b57
brakine elmehdi <elmehdibra@gmail.com>
2023-04-24 12:36:11 -0700

    get_op_func_update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: the operator passed as argument to the program
@@ -8,23 +7,20 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-
-    while (ops[i].op != NULL)
-    {
-        if (*s == *ops[i].op && *(s + 1) == '\0')
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL)
+{
+if (*s == *ops[i].op && *(s + 1) == '\0')
+return (ops[i].f);
+i++;
 }
-
+return (NULL);
+}
