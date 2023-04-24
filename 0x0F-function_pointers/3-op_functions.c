68e1eea6c586023ce09faa38c4dfa47046d31011
brakine elmehdi <elmehdibra@gmail.com>
2023-04-24 12:56:39 -0700

    op_functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,67 @@
 #include "3-calc.h"
+#include <stdlib.h>
 /**
- * op_add - Funcions sum two numbers
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the sum of two numbers
+ * Return: sum of the two integers
  */
 int op_add(int a, int b)
 {
 return (a + b);
 }
 /**
- * op_sub - Funcions subtracion two numbers
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the subtracion of two numbers
+ * Return: difference of the two integers
  */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 /**
- * op_mul - Funcions multiplaction two numbers
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the multiplation of two numbers
+ * Return: product of the two integers
  */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
 /**
- * op_div - Funcions divition two numbers
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the divition of two numbers
+ * Return: result of the division of the two integers
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 /**
- * op_mod - Funcions modulationo of two numbers
+ * op_mod - finds the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the modulation of two numbers
+ * Return: remainder of the division of the two integers
  */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
