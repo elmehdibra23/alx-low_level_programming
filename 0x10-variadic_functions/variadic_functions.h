950b61c4e590aed0075d22df3acd686ba7d665ec
brakine elmehdi <elmehdibra@gmail.com>
2023-04-24 13:27:41 -0700

    print_strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,5 +13,6 @@ void (*func)(va_list);
 } print_fmt_t;
 /* function prototypes */
 int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #endif /* VARIADIC_FUNCTIONS_H */
