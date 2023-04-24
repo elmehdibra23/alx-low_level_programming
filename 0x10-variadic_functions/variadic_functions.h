346eb8582b79ce08d8e638ca5cee260e4ab7447e
brakine elmehdi <elmehdibra@gmail.com>
2023-04-24 13:17:10 -0700

    variadic-funcs

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct print_fmt - A structure to define a print format
+ * @type: The format type (char, integer, float, string, or NULL for undefined)
+ * @func: A function pointer to a function that prints the corresponding type
+ */
+typedef struct print_fmt
+{
+char *type;
+void (*func)(va_list);
+} print_fmt_t;
+/* function prototypes */
+int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* VARIADIC_FUNCTIONS_H */
