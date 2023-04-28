825d0069376d4762c1a24f31ab67429e6abb0a4e
brakine elmehdi <elmehdibra@gmail.com>
2023-04-28 01:29:32 -0700

    adv-task-first-update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-void __attribute__((constructor)) pre_main(void)
+/**
+ * pre_main - A function that prints a message before the main function
+ * is executed.
+ */
+void __attribute__((constructor)) pre_main()
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
