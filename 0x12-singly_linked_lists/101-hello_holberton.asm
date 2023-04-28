378702f8a46f066e2aa679f566275342ac5c22c0
brakine elmehdi <elmehdibra@gmail.com>
2023-04-28 04:03:58 -0700

    hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,32 @@
 ; 101-hello_holberton.asm
 
-; Define the global 'main' symbol so the linker knows where the program entry point is
-global main
-
-; Import the printf function from the C library
-extern printf
-
 section .data
     ; Define the string 'Hello, Holberton', followed by a newline (ASCII 10) and a null terminator (ASCII 0)
-    hello db 'Hello, Holberton', 10, 0
+hello:  db "Hello, Holberton", 10, 0
+    ; Define the format string for printf
+format: db "%s", 0
 
 section .text
-main:
-    ; Prepare arguments for printf function call
-    mov rax, 0                             ; Clear rax register (printf requires rax to be 0 for non-SSE calls)
-    lea rdi, [rel hello]                   ; Load the effective address of the 'hello' string into rdi (1st argument)
+    ; Define the global 'main' symbol so the linker knows where the program entry point is
+    global main
+    ; Import the printf function from the C library
+    extern printf
 
+main:
+    ; Save the current stack frame
+    push rbp
+    mov rbp, rsp
+
+    ; Prepare arguments for printf function call
+    mov rsi, hello                      ; Load the 'hello' string into rsi (2nd argument)
+    mov rdi, format                     ; Load the format string into rdi (1st argument)
     ; Call printf function to print the 'hello' string
     call printf
 
+    ; Restore the previous stack frame
+    pop rbp
+
     ; Exit the program
-    mov rax, 60                            ; Load the 'sys_exit' system call number (60) into rax
-    xor rdi, rdi                           ; Clear rdi register (set it to 0, the exit status of the program)
-    syscall                                ; Make the system call to exit the program
+    mov rax, 0                          ; Set the return value to 0
+    ret                                 ; Return from the 'main' function
 
