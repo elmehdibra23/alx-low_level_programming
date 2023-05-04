73172182b5d3192e5e99c86f4e7987b47003808f
brakine elmehdi <elmehdibra@gmail.com>
2023-05-04 09:54:13 -0700

    binary_to_unit_update1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,42 @@
 #include "main.h"
-#include <stdbool.h>
+
 /**
-* binary_to_uint - converts a binary number to an unsigned int
-* @b: pointer to a string of 0 and 1 chars
-*
-* Return: the converted number, or 0 if
-* there is one or more chars in the string b that is not 0 or 1
-* b is NULL
-*/
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if there is an error
+ */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result = 0;
-int i;
-if (b == NULL)
-{
-return 0;
+	unsigned int decimal_value = 0;
+	int i;
+
+	if (b == NULL)
+	{
+		/* Return 0 if input is NULL */
+		return (0);
+	}
+
+	/* Iterate through the string to check for non-binary characters */
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			/* Return 0 if input contains non-binary characters */
+			return (0);
+		}
+	}
+
+	/* Convert the binary string to decimal using bitwise operations */
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		decimal_value <<= 1;
+		if (b[i] == '1')
+		{
+			decimal_value |= 1;
+		}
+	}
+
+	return (decimal_value);
 }
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] != '0' && b[i] != '1')
-{
-return 0;
-}
-}
-for (i = 0; b[i] != '\0'; i++)
-{
-result <<= 1;
-if (b[i] == '1')
-{
-result |= 1;
-}
-}
-return result;
-}
+
