1ff85df83e08e239df6cf928e26e9432e71071f6
brakine elmehdi <elmehdibra@gmail.com>
2023-05-16 11:37:07 -0700

    cp_update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,57 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include "main.h"
-/**
-* main - Copies the content of a file to another file.
-* @argc: The number of command-line arguments.
-* @argv: An array containing the command-line arguments.
-*
-* Return: 0 on success.
-*/
-int main(int argc, char *argv[])
-{
-int fd_from, fd_to;
+int main(int argc, char *argv[]) {
+FILE *source_file;
+FILE *destination_file;
 char buffer[1024];
-ssize_t bytes_read, bytes_written;
-if (argc != 3)
-{
-dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+size_t bytes_read;
+if (argc != 3) {
+fprintf(stderr, "Usage: cp file_from file_to\n");
 exit(97);
 }
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+source_file = fopen(argv[1], "r");
+if (source_file == NULL) {
+fprintf(stderr, "Error: Unable to read from file %s\n", argv[1]);
 exit(98);
 }
-fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
+destination_file = fopen(argv[2], "w");
+if (destination_file == NULL) {
+fprintf(stderr, "Error: Unable to write to file %s\n", argv[2]);
+fclose(source_file);
 exit(99);
 }
-while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-close(fd_to);
-exit(99);
+while ((bytes_read = fread(buffer, 1, sizeof(buffer), source_file)) > 0) {
+fwrite(buffer, 1, bytes_read, destination_file);
 }
-}
-if (bytes_read == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
-close(fd_from);
-close(fd_to);
-return (0);
+fclose(source_file);
+fclose(destination_file);
+return 0;
 }
