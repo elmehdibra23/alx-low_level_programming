4273415414bd49bacdebd54a55d960a6dc5dd490
brakine elmehdi <elmehdibra@gmail.com>
2023-05-16 12:13:39 -0700

    cp_update_sec_time

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
 char *create_buffer(char *file);
 void close_file(int fd);
 char *create_buffer(char *file)
@@ -16,6 +22,10 @@ exit(99);
 }
 return (buffer);
 }
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
 void close_file(int fd)
 {
 int c = close(fd);
@@ -25,6 +35,18 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
 int from, to, r, w;
