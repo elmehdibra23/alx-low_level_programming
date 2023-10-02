73618876317277fb9707f25455bbdf767513484c
elmehdibra23 <elmehdibra@gmail.com>
2023-10-02 14:58:44 -0700

    updatesss

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,23 +11,18 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t len = 0;
+int i = 0, file;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
-return (-1);
-if (text_content != NULL)
+if (text_content == NULL)
+text_content = "";
+while (text_content[i] != '\0')
 {
-while (text_content[len])
-len++;
-if (write(fd, text_content, len) != len)
-{
-close(fd);
+i++;
+}
+file = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
 return (-1);
-}
-}
-close(fd);
+write(file, text_content, i);
 return (1);
 }
